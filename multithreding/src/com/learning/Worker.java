1794f99e4b6250e8a8946dc7a320b94725f30a1b
Hitesh Kansal <hiteshkansal@outlook.com>
2017-05-17 23:38:24 +0530

    First commit from Eclipse

--- multithreding/src/com/learning/Worker.java
+++ multithreding/src/com/learning/Worker.java
@@ -7,15 +7,15 @@ import java.util.Random;
 public class Worker {
 
 	private Random random = new Random();
-	
+
 	private Object lock1 = new Object();
 	private Object lock2 = new Object();
-	
+
 	private List<Integer> list1 = new ArrayList<Integer>();
 	private List<Integer> list2 = new ArrayList<Integer>();
-	
-	public  void stageOne(){
-		synchronized(lock1){
+
+	public void stageOne() {
+		synchronized (lock1) {
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
@@ -24,9 +24,9 @@ public class Worker {
 			list1.add(random.nextInt(100));
 		}
 	}
-	
-	public synchronized void stageTwo(){
-		synchronized(lock2){
+
+	public synchronized void stageTwo() {
+		synchronized (lock2) {
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
@@ -35,39 +35,40 @@ public class Worker {
 			list2.add(random.nextInt(100));
 		}
 	}
-	
-	public void process(){
-		for(int i=0;i<1000;i++){
+
+	public void process() {
+		for (int i = 0; i < 1000; i++) {
 			stageOne();
 			stageTwo();
 		}
 	}
+
 	public void main() {
 
-		System.out.println("Starting...");
+		System.out.println("Starting Process...");
 		long start = System.currentTimeMillis();
-		//process();
-		Thread t1 = new Thread(new Runnable(){
+		// process();
+		Thread t1 = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
 				process();
 			}
-			
+
 		});
-		
-		Thread t2 = new Thread(new Runnable(){
+
+		Thread t2 = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
 				process();
 			}
-			
+
 		});
-		
+
 		t1.start();
 		t2.start();
-		
+
 		try {
 			t1.join();
 			t2.join();
@@ -76,8 +77,8 @@ public class Worker {
 			e.printStackTrace();
 		}
 		long end = System.currentTimeMillis();
-		System.out.println("Time Taken: "+ (end-start));
-		System.out.println("List1:  "+ list1.size()+ " List2: "+list2.size());
+		System.out.println("Time Taken: " + (end - start));
+		System.out.println("List1:  " + list1.size() + " List2: " + list2.size());
 	}
 
 }
